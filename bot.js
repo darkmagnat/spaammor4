cfcfbe57f2be9c68651c0e89d05e41ca7beda169
darkmagnat <43116040+darkmagnat@users.noreply.github.com>
2018-11-13 22:03:53 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,8 +6,8 @@ client.on('ready', () => {
 
 
 client.on('ready', async() => {
-var server = "508240875072126997"; // ايدي السررفر
-var channel = "508667278137753600";//ايدي الروم
+var server = "502354865998266389"; // ايدي السررفر
+var channel = "511567833273073665";//ايدي الروم
     setInterval(()=>{
     client.guilds.get(server).channels.get(channel).send('Universe Town  , Universe Town , Universe Town , Universe Town , Universe Town , Universe Town , Universe Town , Universe Town , Universe Town , Universe Town ')
     },305);
